d5f5516d7a5b9856cbdb780c2c4251f994558c02
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-10 09:45:05 -0700

    Quick update

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -8,15 +8,17 @@
  * @n: integer
  * @next: pointer to the next node.
  * Description: singly linked list node structure
- * for first python project
+ * for this Holberton python project
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 int check_cycle(listint_t *list);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-#endif
+
+#endif /*LISTS_H*/

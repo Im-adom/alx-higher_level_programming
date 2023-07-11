34475b1ddc48891d6b37fe5c3a6bffae52e1e4f2
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-11 12:15:27 -0700

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for g in range(122, 96, -1):
-    if g % 2 != 0:
-        g = g - 32
-        print("{}".format(chr(g)), end="")
+a = 0
+for c in range(ord('z'), ord('a') -1, -1):
+    print("{}".format(chr(c - a)), end="")
+    a = 32 if a == 0 else 0

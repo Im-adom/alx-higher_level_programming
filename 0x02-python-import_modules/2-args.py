0c1f215dd6ff7d63e199ef1f1b1f4d63def5442b
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-13 10:23:12 -0700

    Updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     count_args = len(sys.argv) - 1
     if count_args == 0:
         print("0 arguments")
-    elif count_Args == 1:
+    elif count_args == 1:
         print("1 argument")
     else:
         print("{} arguments: ".format(count_args))

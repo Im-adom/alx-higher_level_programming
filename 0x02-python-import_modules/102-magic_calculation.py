af0dbcc940a2667cf3c0e34e9910002f22c1ea38
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-13 11:41:29 -0700

    Update

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,8 +4,8 @@ def magic_calculation(a, b):
 
     if a < b:
         c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
+        for g in range(4, 6):
+            c = add(c, g)
             return (c)
         else:
             return(sub(a, b))

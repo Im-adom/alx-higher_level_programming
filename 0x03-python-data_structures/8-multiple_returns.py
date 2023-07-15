a4a71105092a9dcae94902254b235db8facee2d0
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-15 07:03:00 -0700

    Multiple returns update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return (None, )
-    else:
-        return (len(sentence), sentence[0])
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])

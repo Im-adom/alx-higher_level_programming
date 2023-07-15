173950d2f02cad951a1632c83522d8107034639b
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-15 06:07:01 -0700

    Printing matrices

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return (None)
     print("\n".join([" ".join(["{:d}".format(mat_numb)
-        for mat_numb in g]) for g in matrix]))
+                    for mat_numb in g]) for g in matrix]))

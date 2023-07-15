a671d6d88a51673cb1146091af44694c195a620d
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-15 06:15:38 -0700

    Finding the 2 divisibles

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    multiples_2 = []
+    for m in range(len(my_list)):
+        if my_list[m] % 2 == 0:
+            multiples_2.append(True)
+        else:
+            multiple_2.append(False)
+
+        return (multiples_2)

b0d1d66889d51653f5a025b0b2d8134e9bcc2775
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-19 09:18:26 -0700

    Returning weighted averages

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not in my_list:
+        return 0
+
+    up = 0
+    down = 0
+
+    for tup in my_lisy:
+        up += tup[0] * tup[1]
+        down += tup[1]
+
+    return (up / down)

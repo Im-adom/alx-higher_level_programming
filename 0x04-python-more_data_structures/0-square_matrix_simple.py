3916cee0a46dca2da10f0714a0e8607fda97a016
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-19 09:05:30 -0700

    Squaring the matrix - update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    col = len(matrix[0])
-    row = len(matrix)
-
-    matrix_square = [[0] * col for _ in range(row)]
-
-    for x in range(row):
-        for y in range(col):
-            matrix_square[x][y] = matrix[x][y] ** 2
-    return matrix_square
+    newmatrix = []
+    for col in matrix:
+        ans = list(map(lambda x: x**2, col))
+        newmatrix.append(ans)
+    return (newmatrix)

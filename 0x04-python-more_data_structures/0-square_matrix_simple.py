6d9e88594c14992a0060e5806054de319397ac06
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-19 10:02:19 -0700

    Updated square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    newmatrix = matrix.copy()
-
-    for nm in range(len(matrix)):
-        newmatrix[nm] = list(map(lambda nm: nm**2, matrix[nm]))
-
-    return (newmatrix)
+    if matrix is not None:
+        return (list)
+    (map(lambda row: list(map(lambda nm: nm ** 2, row)), matrix))
+    return None

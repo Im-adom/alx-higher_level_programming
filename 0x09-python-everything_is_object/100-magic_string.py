bf4594b47736241b0bb45fd6e65fe22c8263d350
Im-adom <graceamaningkwarteng@gmail.com>
2023-08-08 08:56:27 -0700

    Returning the BestSchool

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.c = getattr(magic_string, 'c', 0) + 1
-    return ("BestSchool, " * (magic_String.c - 1) + "BestSchool")
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_String.n - 1) + "BestSchool")

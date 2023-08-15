68b46c33ede581f599311d26e6c145cecce6e87c
Im-adom <graceamaningkwarteng@gmail.com>
2023-08-15 08:59:37 -0700

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a Student class"""
+
+
+class Student:
+    """This represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing a new Student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """This gets the dictionary presentation of Student.
+
+        If attrs is a list of strings, only attribute names
+        contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved"""
+
+        if (type(attrs) == list and
+                all(type(elem) == str for elem in attrs)):
+            return {g: getattr(self, g) for g in attrs if hasattr(self, g)}
+        return self.__dict__

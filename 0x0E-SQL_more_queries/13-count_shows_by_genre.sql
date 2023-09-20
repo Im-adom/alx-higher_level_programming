ede0d2e929b94e97e243f4a486dd2dda0847c045
Im-adom <graceamaningkwarteng@gmail.com>
2023-09-20 12:12:28 -0700

    sql queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tg.`name` AS `genre`,
        COUNT(*) AS `number_of_shows`
   FROM `tv_genres` AS tg
-       INNER JOIN `tv_show_genres` AS tg
+       INNER JOIN `tv_show_genres` AS tsg
        ON tg.`id` = tg.`genre_id`
  GROUP BY tg.`name`
  ORDER BY `number_of_shows` DESC;
